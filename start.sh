b8d4ec5e269bdd97eceb1d13883741bd4bc4a687
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-09-29 06:27:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/meenakshikutty/Dulquer.git /Dulquer
+  git clone https://github.com/Emilyoftg/Dulquer.git /Dulquer
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Dulquer
