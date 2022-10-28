32c616a73ba247f0e683412adb9b17cf43018b84
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-10-28 22:40:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Emilyoftg/Dulquer.git /Dulquer
+  git clone https://github.com/Emilyoftg/Test.git /Test
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Dulquer
+  git clone $UPSTREAM_REPO /Test
 fi
-cd /Dulquer
+cd /Test
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
