b53833ec43996222f884d301e043d8c0b2fd71ee
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-11-19 21:07:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,7 +67,7 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 DELETE_TIME = int(environ.get('DELETE_TIME', 300))
 START_IMAGE_URL = environ.get('START_IMAGE_URL', "")
 UNAUTHORIZED_CALLBACK_TEXT = (environ.get('UNAUTHORIZED_CALLBACK_TEXT', "oKda"))[:200]
-REDIRECT_TO = (environ.get('REDIRECT_TO', "testofindia"))
+REDIRECT_TO = (environ.get('REDIRECT_TO', "MC_Testrobots"))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
